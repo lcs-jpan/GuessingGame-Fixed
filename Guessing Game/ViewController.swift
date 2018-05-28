2962f58c5d4a2d9a65840fb5c1f692a6668bb1d7
Pan Jason <jpan@lcs-mba3130.lcs.on.ca>
2018-05-28 14:32:08 -0400

    fyufu

--- Guessing Game/ViewController.swift
+++ Guessing Game/ViewController.swift
@@ -10,14 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: Outlest
+   
+    // MARK: Properties
+    let numberToGuess = arc4random_uniform(501) // Generate a random number between 0 and 500
+    
+    // MARK: Overrides
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Let's see what the random actually is
+        print("The number to guess is \(numberToGuess)")
+        
+    }
+    
+    override func didReceiverMemoryWaring {
+        super.didReceiveMemoryWarning()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    // MARK: Actions
+    IBAction func checkGuess(_sender:Any) {
+    
+        print("A guess was made.")
+        print(guessedNumber.text)
+        
+        // First we need to unwrap the optional number
+        if guessdNumber.text == numberToGuess {
+            print(
+        }
     }
 
 
